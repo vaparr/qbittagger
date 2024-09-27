0256042c4234b298b3f80322574d7687f0595517
doctarrwho <70813119+doctarrwho@users.noreply.github.com>
2024-09-27 12:57:50 -0600

    Version 2

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip3 install qbittorrent-api
-cd /home/qbit
-# Start trottling and tagging torrents.. run this often
-python3 ./qbit.py
-# Find torrents ready for delete.. dont run this too often
-#python3 ./qbit.py -d
