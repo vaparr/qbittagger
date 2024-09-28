099ce991860ed0757d27f0df06b982858a8aaf6c
doctarrwho <70813119+doctarrwho@users.noreply.github.com>
2024-09-28 12:57:37 -0600

    Exit if trackers config not present

--- qb-tagger.py
+++ qb-tagger.py
@@ -522,6 +522,11 @@ class TorrentManager:
             sys.exit(1)
 
     def load_trackers(self, tracker_json_path):
+
+        if not os.path.exists(tracker_json_path):
+            print(f"ERROR: Unable to find '{tracker_json_path}'")
+            exit(2)
+            
         with open(tracker_json_path, "r") as read_file:
             return json.load(read_file)
 
