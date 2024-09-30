9c8d4f6cb678bdae66a4d6ce06e356ebec7f201f
vaparr <vaparr@gmail.com>
2024-09-30 17:28:14 -0400

    Update to scan all Torrent locations

--- findmissing.py
+++ findmissing.py
@@ -9,6 +9,7 @@ torrents = qb.torrents_info()
 
 # Set to store unique file paths for faster lookups
 unique_files = set()
+save_paths = set()
 
 def is_hard_link(filename):
     return os.stat(filename).st_nlink > 1
@@ -17,7 +18,7 @@ def is_hard_link(filename):
 for torrent in torrents:
     files = qb.torrents_files(torrent['hash'])
     save_path = torrent['save_path'].replace("/plexmedia/", "/mnt/user/plexmedia/")
-    
+    save_paths.add(save_path)
     # Ensure save_path ends with a '/'
     if not save_path.endswith("/"):
         save_path += "/"
@@ -30,16 +31,18 @@ for torrent in torrents:
 hardlink_count = 0
 
 # Walk through the directory to find files
-for root, _, filenames in os.walk("/mnt/user/plexmedia/downloads/qBittorrent/downloads/"):
-    for file in filenames:
-        full_path = os.path.join(root, file)
+for path in save_paths:
+    print(f"Scanning {path}")
+    for root, _, filenames in os.walk(path):
+        for file in filenames:
+            full_path = os.path.join(root, file)
 
-        # Check for hard links
-        if is_hard_link(full_path):
-            hardlink_count += 1
-            
-        # Print paths not in the set
-        if full_path not in unique_files:
-            print(full_path)
+            # Check for hard links
+            if is_hard_link(full_path):
+                hardlink_count += 1
+                
+            # Print paths not in the set
+            if full_path not in unique_files:
+                print(full_path)
 
 print(f"Hard-linked files: {hardlink_count}")
