40ff839685ff1ade031ff39da344b6e05c0836c0
doctarrwho <70813119+doctarrwho@users.noreply.github.com>
2024-10-09 11:15:21 -0600

    Remove dry-run arg, add remove arg

--- qb-delete.py
+++ qb-delete.py
@@ -14,21 +14,23 @@ print(f"{padding}\n{header}\n{padding}")
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Delete torrents in qBittorrent.")
     parser.add_argument("-c", "--config", default="config.yaml", help="Path to the config file.")
-    parser.add_argument("-d", "--dry-run", default=False, action="store_true", help="Perform a dry run without making changes.")
     parser.add_argument("-n", "--no-color", default=False, action="store_true", help="No color in output. Useful when running in unraid via User scripts.")
     parser.add_argument("-t", "--tag-names", default="#_unregistered", help="Tag name(s, comma-separated) for which to list torrents.")
+    parser.add_argument("-r", "--remove", default=False, action="store_true", help="Remove torrents - explicitly specify to prevent accidental removal.")
 
     args = parser.parse_args()
 
     config_manager = ConfigManager(args.config, None)
     util.Config_Manager = config_manager
 
-    manager = TorrentManager(args.dry_run, args.no_color, None)
+    manager = TorrentManager(dry_run=True, no_color=args.no_color, tracker_json_path=None)
 
     tag_names = [t.strip() for t in args.tag_names.split(",")]
     total_size = 0
     print()
+
     torrents_dict = manager.qb.torrents_info()
+
     for torrent in torrents_dict:
         torrent_tags = [t.strip() for t in torrent.get("tags", "").split(",")]
         # if any(tag in torrent_tags for tag in tag_names):
@@ -39,15 +41,15 @@ if __name__ == "__main__":
             torrent_size = torrent['size']
             total_size += torrent_size
             formatted_size = util.format_bytes(torrent_size)
-            if args.dry_run:
+            if not args.remove:
                 print(f"-- [DRY RUN] Will remove [{matching_tag if args.no_color else f'{Fore.GREEN}{matching_tag}{Fore.RESET}'}] '{torrent_name if args.no_color else f'{Fore.YELLOW}{torrent_name}{Fore.RESET}'}' ({torrent_hash if args.no_color else f'{Fore.CYAN}{torrent_hash}{Fore.RESET}'}) torrent with size '{formatted_size if args.no_color else f'{Fore.GREEN}{formatted_size}{Fore.RESET}'}'")
             else:
-                print(f"-- Removing [{matching_tag if args.no_color else f'{Fore.GREEN}{matching_tag}{Fore.RESET}'}] '{torrent_name if args.no_color else f'{Fore.YELLOW}{torrent_name}{Fore.RESET}'}' ({torrent_hash if args.no_color else f'{Fore.CYAN}{torrent_hash}{Fore.RESET}'}) torrent with size '{formatted_size if args.no_color else f'{Fore.GREEN}{formatted_size}{Fore.RESET}'}'")
                 # remove torrents with delete_files set to False, as orphan cleanup will take care of them.
+                print(f"-- Removing [{matching_tag if args.no_color else f'{Fore.GREEN}{matching_tag}{Fore.RESET}'}] '{torrent_name if args.no_color else f'{Fore.YELLOW}{torrent_name}{Fore.RESET}'}' ({torrent_hash if args.no_color else f'{Fore.CYAN}{torrent_hash}{Fore.RESET}'}) torrent with size '{formatted_size if args.no_color else f'{Fore.GREEN}{formatted_size}{Fore.RESET}'}'")
                 # manager.qb.torrents_delete(delete_files=False, torrent_hashes=torrent_hash)
 
     print()
-    if args.dry_run:
+    if not args.remove:
         print(f"[DRY RUN] Total size of removed torrents with '{tag_names if args.no_color else f'{Fore.GREEN}{tag_names}{Fore.RESET}'}' tag: {util.format_bytes(total_size)}")
     else:
         print(f"Total size of removed torrents with '{tag_names if args.no_color else f'{Fore.GREEN}{tag_names}{Fore.RESET}'}' tag: {util.format_bytes(total_size)}")
